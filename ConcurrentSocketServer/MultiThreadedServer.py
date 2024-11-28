0887af48ee4da2f030e56aa7fd7d34a7a2ec7c25
Dionte hill <diontemhill@gmail.com>
2024-11-28 12:24:29 -0500

    first commit

--- ConcurrentSocketServer/MultiThreadedServer.py
+++ ConcurrentSocketServer/MultiThreadedServer.py
@@ -1,5 +1,6 @@
 import socket
 import subprocess
+import threading
 
 
 def perform_operation(op):
@@ -51,6 +52,46 @@ def perform_operation(op):
 			return result.encode()
 	else:
 		return "invalid request".encode()
+	
+
+def handle_client(client_socket,client_address):
+		print(f"connection from:{client_address} ")
+		with client_socket:
+			while True:
+				request =client_socket.recv(1024)
+				if not request: 
+					print("client disconnected")
+					break 
+
+				try:
+					op=int(request.decode())
+					print(f"Recieved operation request: {op}")
+					
+					if op==7:
+						print("\nshutdown command recieved.")
+						client_socket.sendall("server shutting down..".encode())
+						client_socket.close()
+						return
+					
+					response=perform_operation(op)
+					
+				except ValueError:
+					response="Invalid request format".encode()
+					print("Recieved invalid request format")
+
+				try:
+					client_socket.sendall(response)
+					print("Response sent to the client")
+					
+
+
+				except BrokenPipeError as bp:
+					print(f"Broken Pipe error: Client {client_address} disconnected before response sent")
+					break
+				except Exception as e:
+					print(f"Unexpected error handling client {client_address} {e}")
+					break
+
 
 
 def spin_up():
@@ -59,37 +100,20 @@ def spin_up():
 		print(f"socket bound to port {2998}")
 		s.listen(25)
 
-		while True:
-			client_socket, client_address = s.accept()
-			print(f"Connection from:{client_address}")
-			
-			with client_socket:
-				request = client_socket.recv(1024)
-
-				if not request:
-					print("Client disconnected")
-					continue
-
-				try:
-					op = int(request.decode())
-					print(f"\nReceived operation request: {op}")
-					
-					if op == 7:
-						s.shutdown(socket.SHUT_RDWR)
-						s.close()
-						print("\nServer no longer listening. Shutting down...")
-						return
-					
-					response = perform_operation(op)
-				except ValueError:
-					response = "Invalid request format".encode()
-					print("Received invalid request format")
-
-				try:
-					client_socket.sendall(response)
-					print("\nResponse sent to the client")
-				except BrokenPipeError as bp:
-					print(f"Broken Pipe Error: Client {client_address} disconnected before response sent")
+	while True:
+			try:
+				client_socket, client_address = s.accept()
+				print(f"Connection from:{client_address}")
+				
+				client_thread= threading.Thread(target=handle_client,args=(client_socket,client_address))
+				client_thread.daemon=True
+				client_thread.start()
+			except KeyboardInterrupt:
+				print("\n Servr shutting down")
+				break
+			except Exception as e:
+				print(f"Unexpected error in main server loop: {e}")
+ 
 
 if __name__ == "__main__":
 	spin_up()
